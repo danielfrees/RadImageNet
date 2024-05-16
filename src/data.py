c59942d2314996af5775e2cec176bd618a4bb960
Daniel Frees <danielfrees@g.ucla.edu>
2024-05-16 11:26:58 -0700

    add caffe pretransform, interp, other fixes (v2 pytorch)

--- src/data.py
+++ src/data.py
@@ -5,6 +5,7 @@ from PIL import Image
 import torch
 from torch.utils.data import DataLoader, Dataset
 from torchvision import transforms
+from torchvision.transforms.functional import InterpolationMode
 
 # Custom Dataset class for handling image loading
 class RadDataset(Dataset):
@@ -41,6 +42,31 @@ class RadDataset(Dataset):
             image = self.transform(image)
 
         return image, label
+    
+class CaffeTransform:
+    """ 
+    Mimic the preprocessing of tf.keras.applications.imagenet_utils.preprocess_input 
+    used by the original RadImageNet authors. 
+    
+    Caffe style image preprocessing. Built based on tf docs: 
+    https://github.com/keras-team/keras/blob/v2.9.0/keras/applications/imagenet_utils.py
+    """
+    def __init__(self, mean=[103.939, 116.779, 123.68]):   # set mean to imagenet caffe style means
+        self.mean = torch.tensor(mean).view(3, 1, 1)   # convert to tensor
+        self.mean = self.mean * 1./255   # rescale to apply to Tensor img in range [0,1]
+
+    def __call__(self, img):
+        # Convert from RGB to BGR
+        img = img[[2, 1, 0], :, :]
+        # Subtract mean based on ImageNet
+        img = img - self.mean
+        return img
+    
+class Standardize:
+    """Standardize the image by scaling pixel values by 1/255."""
+    def __call__(self, img):
+        return img / 255.0
+    
 
 def create_dataloaders(train_df, val_df, batch_size: int, image_size: int, partial_path: str) -> tuple[DataLoader, DataLoader]:
     """
@@ -56,25 +82,39 @@ def create_dataloaders(train_df, val_df, batch_size: int, image_size: int, parti
         tuple: (train_loader, val_loader) DataLoaders for the training and validation datasets.
     """
     train_transform = transforms.Compose([
-    transforms.Resize((256, 256)),
+    transforms.Resize((image_size, image_size)),
     transforms.RandomHorizontalFlip(),
-    transforms.RandomRotation(10),
+    transforms.RandomRotation(10, interpolation=InterpolationMode.NEAREST, fill = 128),
     transforms.RandomAffine(
         degrees=0,
-        translate=(0.1, 0.1),
-        scale=(0.9, 1.1),
-        shear=0.1,
-        fill=None,  # use padding_mode
-        padding_mode='edge'  # use the nearest pixel values to fill the empty areas - like TF
+        translate=(0.1, 0.1),  # matches
+        scale=(0.9, 1.1),  # matches
+        shear=0.1,   # matches https://github.com/keras-team/keras/blob/v3.1.0/keras/legacy/preprocessing/image.py
     ),
     transforms.ToTensor(),
-    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    CaffeTransform(),   # mimic caffe style preprocess_image fnc from tensorflow
+    #Standardize(),   # *1./255 (handled implicitly by ToTensor())
+    # BGR
+    # transforms.Normalize(mean=[0.406, 0.456, 0.485], std=[0.225, 0.224, 0.229])
+
+    # BGR with no mean (caffe already does the mean)
+    transforms.Normalize(mean=[0, 0, 0], std=[0.225, 0.224, 0.229])
+
+    # RGB
+    # transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 ])
 
     val_transform = transforms.Compose([
         transforms.Resize((image_size, image_size)),
         transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+        CaffeTransform(),   # mimic caffe style preprocess_image fnc from tensorflow
+        #Standardize(),  # *1./255 (handled implicitly by ToTensor())
+        #BGR
+        #transforms.Normalize(mean=[0.406, 0.456, 0.485], std=[0.225, 0.224, 0.229])
+        # BGR with no mean (caffe already does the mean)
+        transforms.Normalize(mean=[0, 0, 0], std=[0.225, 0.224, 0.229])
+        #RGB
+        # transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
     ])
 
     train_dataset = RadDataset(train_df, transform=train_transform, partial_path=partial_path)
